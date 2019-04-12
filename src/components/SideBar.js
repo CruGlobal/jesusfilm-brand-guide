dd9f64096c5238ca72cd7727d3011ce7781491ff
Gavin Walsh <walsh.gavin@gmail.com>
2019-04-12 13:40:58 -0400

    assets

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
-import logo from "../img/logo.svg";
+import logo from "../../static/img/jfm-logo.svg";
 
 const SideBar = ({isActive}) => (
       <aside className="menu" style={{margin: 10, marginTop: 25}}>
         <Link to="/" title="Logo">
-          <img src={logo} alt="JFP" style={{ width: 88 }} />
+          <img src={logo} alt="JFP" style={{ width: 150 }} />
         </Link>
         <p className="menu-label">
           Culture
@@ -36,13 +36,13 @@ const SideBar = ({isActive}) => (
         </p>
         <ul className="menu-list">
           <li>
-            <a>General</a>
+            <a href="#">General</a>
             <ul>
               <li><Link to="/style-guide/general/color">Colors</Link></li>
             </ul>
           </li>
           <li>
-            <a>Web</a>
+            <a href="#">Web</a>
             <ul>
               <li><Link to="/style-guide/web/typography">Typography</Link></li>
               <li><Link to="/style-guide/web/buttons">Buttons</Link></li>
